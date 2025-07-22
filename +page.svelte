7c8505eb4f031bcd7bc2d7428977f2d3551d2ba7
Nasrin Ali <53440444+nasrina081@users.noreply.github.com>
2025-07-21 20:14:29 -0700

    Update +page.svelte
    testing out my first svelte transition

---
+++ +page.svelte
@@ -0,0 +1,13 @@
+<script>
+// testing out a svelte transition
+import { fade, fly } from 'svelte/transition'
+
+// keeping track of which sections can be seen on scroll
+let sectionSeenOnScroll = {}
+
+// making a section invisible
+const reveal = (id) => {
+  sectionSeenOnScroll = { ...sectionSeenOnScroll, [id]: true}
+}
+</script>
+
